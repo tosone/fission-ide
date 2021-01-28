1f3cc9be58504141c85dd827014efd0a2c83200c
Tosone <i@tosone.cn>
2021-01-29 00:43:50 +0800

    :heavy_plus_sign: Add antd and react.

---
+++ ide/build/webpack.view.js
@@ -0,0 +1,46 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        configViewer: "./src/view/app/index.tsx"
+    },
+    output: {
+        path: path.resolve(__dirname, "configViewer"),
+        filename: "[name].js"
+    },
+    devtool: "eval-source-map",
+    resolve: {
+        extensions: [".js", ".ts", ".tsx", ".json"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                loader: "ts-loader",
+                options: {}
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ]
+    },
+    performance: {
+        hints: false
+    }
+};
